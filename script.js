0db5fa379788b3f9da89b20e43dc92a3b8750d1c
Memelly12 <memelelie123@gmail.com>
2024-02-16 19:08:27 +0000

    fixer des bugs dans la fonction game et creer une fonction play

--- script.js
+++ script.js
@@ -1,25 +1,63 @@
 function getComputerChoice() {
   let computerChoice = ["rock", "paper", "scissors"];
+
   let rand = Math.floor(Math.random() * computerChoice.length);
+
   let computerValue = computerChoice[rand];
+
   console.log(computerValue);
+  return computerValue;
 }
-function game(playerchoice, computerGame) {
-  console.log(`player choice : ${playerChoice} `);
-  console.log(`opponent choice : ${computerGame} `);
+
+function game(playerChoice, computerGame) {
+  console.log(`player choice : ${playerChoice}`);
+
+  console.log(`opponent choice : ${computerGame}`);
+  var result;
+
   let win =
-    (playerChoice == "rock" && computerGame == "scissors") ||
-    (playerChoice == "paper" && computerGame == "rock") ||
-    (playerChoice == "scissors" && computerGame == "paper");
+    (playerChoice === "rock" && computerGame === "scissors") ||
+    (playerChoice === "paper" && computerGame === "rock") ||
+    (playerChoice === "scissors" && computerGame === "paper");
+
   if (win) {
-    console.log("you win");
-  } else if (playerChoice == computerGame) {
-    console.log("it's a tie");
+    result = "you win";
+  } else if (playerChoice === computerGame) {
+    result = "it's tie";
   } else {
-    console.log("you lose");
+    result = "you lose";
+  }
+
+  return result;
+}
+
+function playGame() {
+  let W = 0;
+  let T = 0;
+  let L = 0;
+  for (let i = 0; i <= 4; i++) {
+    var playerChoice = prompt("faites votre choix").toLowerCase();
+    var computerGame = getComputerChoice();
+    resultRound = game(playerChoice, computerGame);
+    if (resultRound == "you win") {
+      W = W + 1;
+    } else if (resultRound == "you lose") {
+      L = L + 1;
+    } else if (resultRound == "it's tie") {
+      T = T + 1;
+    }
+    console.log(resultRound);
+  }
+  console.log(`nombre de victoires: ${W} victoires`);
+  console.log(`nombre de defaites: ${L} défaites`);
+  console.log(`nombre de nuls: ${T} nuls`);
+  if (W > L) {
+    console.log("vous avez gagnez le jeu");
+  } else if (W < L) {
+    console.log("vous avez perdu le jeu ");
+  } else {
+    console.log("match nul ");
   }
 }
-let playerChoice = toLowercase(prompt("quelle est votre choix "));
-let computerGame = getComputerChoice();
 
-game(playerChoice, computerGame);
+playGame();
