6887301af3ceb4845f469c0c017d0b25258c7883
Memelly12 <memelelie123@gmail.com>
2024-02-19 15:39:39 +0000

    modifification du script de sorte a ajouter une interface graphique et ajout de stylisation à notre page

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+const canvaComputer = document.querySelector("#canvaComputer");
+const canvaPlayer = document.querySelector("#canvaPlayer");
+const choice = document.querySelectorAll("button");
+const canvaPchoice = document.querySelector("#canvaPchoice");
+const canvaCchoice = document.querySelector("#canvaCchoice");
+const resulFinal = document.querySelector("#resultFinal");
+let W = 0;
+let T = 0;
+let L = 0;
+let playerChoice;
+var computerGame;
+var resultRound;
+
 function getComputerChoice() {
   let computerChoice = ["rock", "paper", "scissors"];
 
   let rand = Math.floor(Math.random() * computerChoice.length);
 
   let computerValue = computerChoice[rand];
+  canvaCchoice.setAttribute("src", `images/${computerValue}.png`);
 
   console.log(computerValue);
   return computerValue;
@@ -22,9 +36,13 @@ function game(playerChoice, computerGame) {
 
   if (win) {
     result = "you win";
+    canvaComputer.style.backgroundColor = "red";
+    canvaPlayer.style.backgroundColor = "green";
   } else if (playerChoice === computerGame) {
     result = "it's tie";
   } else {
+    canvaComputer.style.backgroundColor = "green";
+    canvaPlayer.style.backgroundColor = "red";
     result = "you lose";
   }
 
@@ -32,13 +50,26 @@ function game(playerChoice, computerGame) {
 }
 
 function playGame() {
-  let W = 0;
-  let T = 0;
-  let L = 0;
-  for (let i = 0; i <= 4; i++) {
-    var playerChoice = prompt("faites votre choix").toLowerCase();
-    var computerGame = getComputerChoice();
+  if (W + L + T == 5) {
+    console.log(`nombre de victoires: ${W} victoires`);
+    console.log(`nombre de defaites: ${L} défaites`);
+    console.log(`nombre de nuls: ${T} nuls`);
+    if (W > L) {
+      console.log("vous avez gagnez le jeu");
+    } else if (W < L) {
+      console.log("vous avez perdu le jeu ");
+    } else {
+      console.log("match nul ");
+    }
+  }
+}
+choice.forEach((button) => {
+  button.addEventListener("click", () => {
+    playerChoice = button.id;
+    canvaPchoice.setAttribute("src", `images/${playerChoice}.png`);
+    computerGame = getComputerChoice();
     resultRound = game(playerChoice, computerGame);
+    console.log(resultRound);
     if (resultRound == "you win") {
       W = W + 1;
     } else if (resultRound == "you lose") {
@@ -46,18 +77,7 @@ function playGame() {
     } else if (resultRound == "it's tie") {
       T = T + 1;
     }
-    console.log(resultRound);
-  }
-  console.log(`nombre de victoires: ${W} victoires`);
-  console.log(`nombre de defaites: ${L} défaites`);
-  console.log(`nombre de nuls: ${T} nuls`);
-  if (W > L) {
-    console.log("vous avez gagnez le jeu");
-  } else if (W < L) {
-    console.log("vous avez perdu le jeu ");
-  } else {
-    console.log("match nul ");
-  }
-}
+  });
+});
 
 playGame();
